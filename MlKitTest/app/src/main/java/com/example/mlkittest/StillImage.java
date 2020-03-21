f01dfd4c5a7e3190740089c49b71fa53b6fc6c1e
anupam <anupam.bhardwaj@tothenew.com>
2020-03-21 14:30:06 +0530

    New Commit

---
+++ MlKitTest/app/src/main/java/com/example/mlkittest/StillImage.java
@@ -0,0 +1,30 @@
+package com.example.mlkittest;
+
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class StillImage extends Fragment {
+
+    public static StillImage newInstance() {
+        return new StillImage();
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_still_image, container, false);
+    }
+}
